f47e35f0b563d4cee200a3446070e89f4cd7ffa4
Jacob Hailouni <jacob.hailouni@gmail.com>
2026-01-19 09:47:59 +0400

    mobile Menu fix

--- src/components/navigation/MobileMenu.tsx
+++ src/components/navigation/MobileMenu.tsx
@@ -7,18 +7,24 @@ interface MenuState {
 }
 
 const MobileMenu: React.FC = () => {
-  const [isSubActive, setSubIsActive] = useState<MenuState>({ status: false, key: "" });
-  const [isActive, setIsActive] = useState<MenuState>({ status: false, key: "" });
+  const [isSubActive, setSubIsActive] = useState<MenuState>({
+    status: false,
+    key: "",
+  });
+  const [isActive, setIsActive] = useState<MenuState>({
+    status: false,
+    key: "",
+  });
 
   const handleClick = (key: string) => {
-    setIsActive(prev =>
-      prev.key === key ? { status: false, key: "" } : { status: true, key }
+    setIsActive((prev) =>
+      prev.key === key ? { status: false, key: "" } : { status: true, key },
     );
   };
 
   const handleSubClick = (key: string) => {
-    setSubIsActive(prev =>
-      prev.key === key ? { status: false, key: "" } : { status: true, key }
+    setSubIsActive((prev) =>
+      prev.key === key ? { status: false, key: "" } : { status: true, key },
     );
   };
 
@@ -26,7 +32,7 @@ const MobileMenu: React.FC = () => {
     <ul className="navigation">
       <li className="dropdown">
         <Link href="/">Home</Link>
-        <ul className={isActive.key === "home" ? "sub-menu d-block" : "d-none"}>
+        {/* <ul className={isActive.key === "home" ? "sub-menu d-block" : "d-none"}>
           <li><Link href="/">Home Page 1</Link></li>
           <li><Link href="/index-2">Home Page 2</Link></li>
           <li><Link href="/index-3">Home Page 3</Link></li>
@@ -34,7 +40,7 @@ const MobileMenu: React.FC = () => {
           <li><Link href="/index-5">Home Page 5</Link></li>
           <li><Link href="/index-6">Home Page 6</Link></li>
           <li><Link href="/index-7">Home Page 7</Link></li>
-        </ul>
+        </ul> */}
         <div
           className={`dropdown-btn ${isActive.key === "home" ? "active" : ""}`}
           onClick={() => handleClick("home")}
@@ -43,15 +49,26 @@ const MobileMenu: React.FC = () => {
           <i className="fa fa-angle-down" />
         </div>
       </li>
-      <li className="dropdown">
+          <li>
+            <Link href="/page-about">About Us</Link>
+          </li>
+      {/* <li className="dropdown">
         <Link href="#">Pages</Link>
-        <ul className={isActive.key === "pages" ? "sub-menu d-block" : "d-none"}>
-          <li><Link href="/page-about">About Us</Link></li>
+        <ul
+          className={isActive.key === "pages" ? "sub-menu d-block" : "d-none"}
+        >
+          <li>
+            <Link href="/page-about">About Us</Link>
+          </li>
           <li>
             <Link href="#">Team</Link>
             <ul className={isSubActive.key === "team" ? "d-block" : "d-none"}>
-              <li><Link href="/page-team">Team Grid</Link></li>
-              <li><Link href="/page-team-details">Team Details</Link></li>
+              <li>
+                <Link href="/page-team">Team Grid</Link>
+              </li>
+              <li>
+                <Link href="/page-team-details">Team Details</Link>
+              </li>
             </ul>
             <div
               className={`dropdown-btn ${isSubActive.key === "team" ? "active" : ""}`}
@@ -61,17 +78,33 @@ const MobileMenu: React.FC = () => {
               <i className="fa fa-angle-down" />
             </div>
           </li>
-          <li><Link href="/page-testimonial">Testimonials</Link></li>
-          <li><Link href="/page-faq">FAQ’s</Link></li>
-          <li><Link href="/page-pricing">Pricing Table</Link></li>
+          <li>
+            <Link href="/page-testimonial">Testimonials</Link>
+          </li>
+          <li>
+            <Link href="/page-faq">FAQ’s</Link>
+          </li>
+          <li>
+            <Link href="/page-pricing">Pricing Table</Link>
+          </li>
           <li>
             <Link href="#">Shop</Link>
             <ul className={isSubActive.key === "shop" ? "d-block" : "d-none"}>
-              <li><Link href="/shop-products">Shop Product</Link></li>
-              <li><Link href="/shop-products-sidebar">Products Sidebar</Link></li>
-              <li><Link href="/shop-product-details">Product Details</Link></li>
-              <li><Link href="/shop-checkout">Checkout</Link></li>
-              <li><Link href="/shop-cart">Cart</Link></li>
+              <li>
+                <Link href="/shop-products">Shop Product</Link>
+              </li>
+              <li>
+                <Link href="/shop-products-sidebar">Products Sidebar</Link>
+              </li>
+              <li>
+                <Link href="/shop-product-details">Product Details</Link>
+              </li>
+              <li>
+                <Link href="/shop-checkout">Checkout</Link>
+              </li>
+              <li>
+                <Link href="/shop-cart">Cart</Link>
+              </li>
             </ul>
             <div
               className={`dropdown-btn ${isSubActive.key === "shop" ? "active" : ""}`}
@@ -81,7 +114,9 @@ const MobileMenu: React.FC = () => {
               <i className="fa fa-angle-down" />
             </div>
           </li>
-          <li><Link href="/page-404">404 Error</Link></li>
+          <li>
+            <Link href="/page-404">404 Error</Link>
+          </li>
         </ul>
         <div
           className={`dropdown-btn ${isActive.key === "pages" ? "active" : ""}`}
@@ -90,12 +125,20 @@ const MobileMenu: React.FC = () => {
         >
           <i className="fa fa-angle-down" />
         </div>
-      </li>
+      </li> */}
       <li className="dropdown">
         <Link href="#">Services</Link>
-        <ul className={isActive.key === "services" ? "sub-menu d-block" : "d-none"}>
-          <li><Link href="/page-services">Service Grid</Link></li>
-          <li><Link href="/page-service-details">Service Details</Link></li>
+        <ul
+          className={
+            isActive.key === "services" ? "sub-menu d-block" : "d-none"
+          }
+        >
+          <li>
+            <Link href="/page-services">Service Grid</Link>
+          </li>
+          <li>
+            <Link href="/page-service-details">Service Details</Link>
+          </li>
         </ul>
         <div
           className={`dropdown-btn ${isActive.key === "services" ? "active" : ""}`}
@@ -107,9 +150,17 @@ const MobileMenu: React.FC = () => {
       </li>
       <li className="dropdown">
         <Link href="#">Projects</Link>
-        <ul className={isActive.key === "projects" ? "sub-menu d-block" : "d-none"}>
-          <li><Link href="/page-project">Projects</Link></li>
-          <li><Link href="/page-project-details">Project Details</Link></li>
+        <ul
+          className={
+            isActive.key === "projects" ? "sub-menu d-block" : "d-none"
+          }
+        >
+          <li>
+            <Link href="/page-project">Projects</Link>
+          </li>
+          <li>
+            <Link href="/page-project-details">Project Details</Link>
+          </li>
         </ul>
         <div
           className={`dropdown-btn ${isActive.key === "projects" ? "active" : ""}`}
@@ -122,8 +173,12 @@ const MobileMenu: React.FC = () => {
       <li className="dropdown">
         <Link href="#">Blog</Link>
         <ul className={isActive.key === "blog" ? "sub-menu d-block" : "d-none"}>
-          <li><Link href="/news-grid">Blog Grid</Link></li>
-          <li><Link href="/news-details">Blog Details</Link></li>
+          <li>
+            <Link href="/news-grid">Blog Grid</Link>
+          </li>
+          <li>
+            <Link href="/news-details">Blog Details</Link>
+          </li>
         </ul>
         <div
           className={`dropdown-btn ${isActive.key === "blog" ? "active" : ""}`}
